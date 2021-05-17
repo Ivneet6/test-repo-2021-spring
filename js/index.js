6d8e686344522eb17e634b0156ebedc3de2466e3
Ivneet Kaur Raina <84252408+Ivneet6@users.noreply.github.com>
2021-05-17 14:53:28 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 
+//TASK 1
+
+/*Declare three variables, a and b and c.*/
+
+
+
+let a = 3;
+let b = 5;
+let c;
+
+let sum = a + b;
+let sub = a - b;
+let multi = a * b;
+let divide = a / b;
+let remainder = a % b
+
+
+
+let output = 'let a = 3;\nlet b = 5;\nlet c\n-----------\n';
+
+
+
+output += 'a + b = ' + (sum) +'\n';
+output += 'a - b = ' + (sub) +'\n';
+output += 'a * b = ' + (multi) +'\n';
+output += 'a / b = ' + (divide) +'\n';
+output += 'a % b = ' + (remainder) +'\n';
+output += 'a += b = ' + (sum) +'\n';
+output += 'a -= b = ' + (a) +'\n';
+output += 'a *= b = ' + (multi) +'\n';
+output += 'a /= b = ' + (a) +'\n';
+output += 'a %= b = ' + (a) +'\n';
+output += 'a == b: ' + (a==b) +'\n';
+output += 'a != b: ' + (a!=b) +'\n';
+output += 'a > b: ' + (a > b) +'\n';
+output += 'a < b: ' + (a < b) +'\n';
+output += '!a && !c: ' + (!a && !c) +'\n'/;
+output += '!a || !c: ' + (!a || !c) +'\n';
+
+
+alert(output); 
